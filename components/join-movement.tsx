c8b71b0966fb78220e29579a5c2035d30b1e1e57
Caleb <92544582+Oluwaferanmi-Dev@users.noreply.github.com>
2025-03-15 17:10:38 +0100

    updated the links

--- components/join-movement.tsx
+++ components/join-movement.tsx
@@ -22,7 +22,7 @@ export function JoinMovement() {
             managers everywhere who are redefining cleanliness and efficiency.
           </p>
           <div>
-            <Link href="https://limpiar.online/landingpage">
+            <Link href="https://sales.limpiar.online/home--limpiar-page">
               <Button size="lg" variant="secondary" className="text-blue-500 hover:text-blue-600">
                 Get Started
               </Button>
